23cb1bdf3ab43f296b249b8386b446acf20361d9
ksainc <krupinski01@gmail.com>
2023-09-22 19:30:53 -0400

    Initial Skeleton

---
+++ lib/Service/NoteNotFound.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+// SPDX-FileCopyrightText: Sebastian Krupinski <krupinski01@gmail.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace OCA\Data\Service;
+
+class NoteNotFound extends \Exception {
+}
