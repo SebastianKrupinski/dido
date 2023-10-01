bfc824ae0b62837bb341b82a5f61388f29102da2
ksainc <krupinski01@gmail.com>
2023-09-30 20:11:00 -0400

    Basic UI Implementation

--- src/UserSettings.vue
+++ src/UserSettings.vue
@@ -22,138 +22,76 @@
 -->
 
 <template>
-	<div id="ds_settings" class="section">
-		<div class="ds-section-heading">
-			<EwsIcon :size="32" /><h2> {{ t('data_service', 'Data Service') }}</h2>
-		</div>
-		<div class="ds-content">
-			<div class="settings-hint">
-				{{ t('data_service', 'List of provisioned services') }}
-			</div>
-			<div>
+	<div>
+		<NcButton @click="alert('I am an alert box!')">
+			Show Settings
+		</NcButton>
 
-			</div>
-			<div>
-				<ul>
-					<li v-for="ritem in availableRemoteContactCollections" :key="ritem.id" class="ds-collectionlist-item">
-						<ContactIcon />
-						<label>
-							{{ ritem.name }} ({{ ritem.count }} Contacts)
-						</label>
-						<NcActions>
-							<template #icon>
-								<LinkIcon />
-							</template>
-							<NcActionButton @click="clearContactCorrelation(ritem.id)">
-								<template #icon>
-									<CloseIcon />
-								</template>
-								Clear
-							</NcActionButton>
-							<NcActionRadio v-for="litem in availableLocalContactCollections"
-								:key="litem.id"
-								:disabled="establishedContactCorrelationDisable(ritem.id, litem.id)"
-								:checked="establishedContactCorrelationSelect(ritem.id, litem.id)"
-								@change="changeContactCorrelation(ritem.id, litem.id)">
-								{{ litem.name }}
-							</NcActionRadio>
-						</NcActions>
-					</li>
-				</ul>
-			</div>
-			<br>
-		</div>
+		<NcAppSettingsDialog :open.sync="ServiceSettingsDialog" name="Application settings">
+			<NcAppSettingsSection id="asci-name-1" name="Example name 1">
+				Some example content
+			</NcAppSettingsSection>
+			<NcAppSettingsSection id="asci-name-2" name="Example name 2">
+				Some more content
+			</NcAppSettingsSection>
+			<NcAppSettingsSection id="asci-name-3" name="Example name 3">
+				Some example content
+			</NcAppSettingsSection>
+			<NcAppSettingsSection id="asci-name-4" name="Example name 4">
+				Some more content
+			</NcAppSettingsSection>
+			<NcAppSettingsSection id="asci-name-5" name="Example name 5">
+				Some example content
+			</NcAppSettingsSection>
+			<NcAppSettingsSection id="asci-name-6" name="Example name 6">
+				Some more content
+			</NcAppSettingsSection>
+			<NcAppSettingsSection id="asci-name-7" name="Example name 7">
+				Some example content
+			</NcAppSettingsSection>
+			<NcAppSettingsSection id="asci-name-8" name="Example name 8">
+				Some more content
+			</NcAppSettingsSection>
+			<NcAppSettingsSection id="asci-name-9" name="Example name 9">
+				Some more content
+			</NcAppSettingsSection>
+			<NcAppSettingsSection id="asci-name-10" name="Example name 10">
+				Some more content
+			</NcAppSettingsSection>
+		</NcAppSettingsDialog>
 	</div>
 </template>
 
 <script>
-import axios from '@nextcloud/axios'
-import { generateUrl } from '@nextcloud/router'
-import { loadState } from '@nextcloud/initial-state'
-import { showSuccess, showError } from '@nextcloud/dialogs'
-
-import NcActions from '@nextcloud/vue/dist/Components/NcActions.js'
-import NcActionButton from '@nextcloud/vue/dist/Components/NcActionButton.js'
-import NcActionRadio from '@nextcloud/vue/dist/Components/NcActionRadio.js'
+import NcAppSettingsDialog from '@nextcloud/vue/dist/Components/NcAppSettingsDialog.js'
+import NcAppSettingsSection from '@nextcloud/vue/dist/Components/NcAppSettingsSection.js'
 import NcButton from '@nextcloud/vue/dist/Components/NcButton.js'
-import NcCheckboxRadioSwitch from '@nextcloud/vue/dist/Components/NcCheckboxRadioSwitch.js'
-import NcSelect from '@nextcloud/vue/dist/Components/NcSelect.js'
-
-import EwsIcon from './icons/EwsIcon.vue'
-import CheckIcon from 'vue-material-design-icons/Check.vue'
-import CloseIcon from 'vue-material-design-icons/Close.vue'
-import CalendarIcon from 'vue-material-design-icons/Calendar.vue'
-import ContactIcon from 'vue-material-design-icons/ContactsOutline.vue'
-import LinkIcon from 'vue-material-design-icons/Link.vue'
 
 export default {
 	name: 'UserSettings',
 
 	components: {
-		NcActions,
-		NcActionButton,
-		NcActionRadio,
+		NcAppSettingsDialog,
+		NcAppSettingsSection,
 		NcButton,
-		NcCheckboxRadioSwitch,
-		NcSelect,
-		EwsIcon,
-		CheckIcon,
-		CloseIcon,
-		CalendarIcon,
-		ContactIcon,
-		LinkIcon,
 	},
 
 	props: [],
 
 	data() {
 		return {
-			readonly: true,
-			state: loadState('data_service', 'user-configuration'),
+			ServiceSettingsDialog: false,
 		}
 	},
 
-	computed: {
-	},
-
-	watch: {
-	},
-
-	mounted() {
-		this.loadData()
-	},
-
 	methods: {
-		loadData() {
-		},
-		onConnectAlternateClick() {
-			const uri = generateUrl('/apps/data_service/connect-alternate')
-			const data = {
-				params: {
-					account_id: this.state.account_id,
-					account_secret: this.state.account_secret,
-					account_server: this.state.account_server,
-					flag: this.configureMail,
-				},
-			}
-			axios.get(uri, data)
-				.then((response) => {
-					if (response.data === 'success') {
-						showSuccess(('Successfully connected to EWS account'))
-						this.state.account_connected = '1'
-						this.fetchPreferences()
-						this.loadData()
-					}
-				})
-				.catch((error) => {
-					showError(
-						t('data_service', 'Failed to authenticate with EWS server')
-						+ ': ' + error.response?.request?.responseText
-					)
-				})
-		},
-
-		onSaveClick() {
+		methods: {
+			openServiceSettings() {
+				this.ServiceSettingsDialog = true
+			},
+			closeServiceSettings() {
+				this.ServiceSettingsDialog = false
+			},
 		},
 	},
 }
@@ -164,43 +102,6 @@ export default {
 	.ds-section-heading {
 		display:inline-block;
 		vertical-align:middle;
-	}
-
-	.ds-connected {
-		display: flex;
-		align-items: center;
-
-		label {
-			padding-left: 1em;
-			padding-right: 1em;
-		}
-	}
-
-	.ds-collectionlist-item {
-		display: flex;
-		align-items: center;
-
-		label {
-			padding-left: 1em;
-			padding-right: 1em;
-		}
-	}
-
-	.ds-actions {
-		display: flex;
-		align-items: center;
-	}
-
-	.external-label {
-		display: flex;
-		//width: 100%;
-		margin-top: 1rem;
-	}
-
-	.external-label label {
-		padding-top: 7px;
-		padding-right: 14px;
-		white-space: nowrap;
 	}
 }
 </style>
